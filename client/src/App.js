079382f72ef08c10b8a8a208a8e6ac170e6c9ca4
Dennis Padiernos <depadiernos@gmail.com>
2019-11-07 20:49:54 -0600

    add AddMovie route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import UpdateMovie from "./Movies/UpdateMovie";
+import AddMovie from "./Movies/AddMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -22,7 +23,8 @@ const App = () => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
       />
-      <Route path="/update-movie/:id" component={UpdateMovie} />
+      <Route path="/update-movie/:id" component={UpdateMovie} />{" "}
+      <Route path="/add-movie/" component={AddMovie} />
     </>
   );
 };
